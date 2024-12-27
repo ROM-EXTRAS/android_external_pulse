40b2d8e782d6c62c33081fde43d84be628b5e48c
Pranav Vashi <neobuddy89@gmail.com>
2024-12-27 12:11:46 +0000

    Pulse: Simplify checks and prevent issues Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I01d2206c463cf88bd40a9fe8cbb0bcce01142583

--- src/com/android/systemui/navigation/pulse/PulseControllerImpl.java
+++ src/com/android/systemui/navigation/pulse/PulseControllerImpl.java
@@ -104,13 +104,12 @@ public class PulseControllerImpl implements
     private boolean mNavPulseEnabled;
     private boolean mLsPulseEnabled;
     private boolean mAmbPulseEnabled;
+    private boolean mPulseEnabled;
+
     private boolean mKeyguardShowing;
     private boolean mDozing;
     private boolean mKeyguardGoingAway;
     private boolean mRenderLoadedOnce;
-
-    private boolean mNavPulseAttached;
-    private boolean mLsPulseAttached;
 
     private final BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
         @Override
@@ -193,7 +192,7 @@ public class PulseControllerImpl implements
                     || uri.equals(Settings.Secure.getUriFor(Settings.Secure.LOCKSCREEN_PULSE_ENABLED))
                     || uri.equals(Settings.Secure.getUriFor(Settings.Secure.AMBIENT_PULSE_ENABLED))) {
                 updateEnabled();
-                updatePulseVisibility(false);
+                updatePulseVisibility();
             } else if (uri.equals(Settings.Secure.getUriFor(Settings.Secure.PULSE_RENDER_STYLE))) {
                 updateRenderMode();
                 loadRenderer();
@@ -212,6 +211,7 @@ public class PulseControllerImpl implements
                     Settings.Secure.LOCKSCREEN_PULSE_ENABLED, 0, UserHandle.USER_CURRENT) == 1;
             mAmbPulseEnabled = Settings.Secure.getIntForUser(mContext.getContentResolver(),
                     Settings.Secure.AMBIENT_PULSE_ENABLED, 0, UserHandle.USER_CURRENT) == 1;
+            mPulseEnabled = mNavPulseEnabled || mLsPulseEnabled || mAmbPulseEnabled;
         }
 
         void updateRenderMode() {
@@ -223,17 +223,17 @@ public class PulseControllerImpl implements
     public void notifyKeyguardGoingAway() {
         if (mLsPulseEnabled) {
             mKeyguardGoingAway = true;
-            updatePulseVisibility(false);
+            updatePulseVisibility();
             mKeyguardGoingAway = false;
         }
     }
 
     public void onStartedGoingToSleep() {
         mScreenOn = false;
-        updatePulseVisibility(true);
+        updatePulseVisibility();
     }
 
-    private void updatePulseVisibility(boolean forceStop) {
+    private void updatePulseVisibility() {
         if (mStatusbar == null) return;
 
         NavigationBarFrame nv = mStatusbar.getNavigationBarView() != null ?
@@ -248,34 +248,24 @@ public class PulseControllerImpl implements
 
 
         if (mKeyguardGoingAway) {
-            if (mLsPulseAttached) {
-                detachPulseFrom(vv, allowNavPulse/*keep linked*/);
-                mLsPulseAttached = false;
-            }
-            return;
-        }
-
-        if (!allowNavPulse && mNavPulseAttached) {
-            detachPulseFrom(nv, allowLsPulse || allowAmbPulse/*keep linked*/);
-            mNavPulseAttached = false;
-        }
-        if (!allowLsPulse && !allowAmbPulse && mLsPulseAttached) {
             detachPulseFrom(vv, allowNavPulse/*keep linked*/);
-            mLsPulseAttached = false;
-        }
-        if ((allowLsPulse || allowAmbPulse) && !mLsPulseAttached) {
-            attachPulseTo(vv);
-            mLsPulseAttached = true;
-        } else if (allowNavPulse && !mNavPulseAttached) {
+        } else if (allowNavPulse) {
+            detachPulseFrom(vv, allowNavPulse/*keep linked*/);
             attachPulseTo(nv);
-            mNavPulseAttached = true;
+        } else if (allowLsPulse || allowAmbPulse) {
+            detachPulseFrom(nv, allowLsPulse || allowAmbPulse/*keep linked*/);
+            attachPulseTo(vv);
+        } else {
+            detachPulseFrom(nv, false /*keep linked*/);
+            detachPulseFrom(vv, false /*keep linked*/);
         }
     }
 
     public void setDozing(boolean dozing) {
         if (mDozing != dozing) {
             mDozing = dozing;
-            updatePulseVisibility(false);
+            if (mPulseEnabled)
+                updatePulseVisibility();
         }
     }
 
@@ -285,7 +275,8 @@ public class PulseControllerImpl implements
             if (mRenderer != null) {
                 mRenderer.setKeyguardShowing(showing);
             }
-            updatePulseVisibility(false);
+            if (mPulseEnabled)
+                updatePulseVisibility();
         }
     }
 
